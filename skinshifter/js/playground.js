e9940cce6fa0e7a376e31b2e688ee95e1436b33d
Ben Rathbun <benr@benrathbun.com>
2015-01-27 13:22:22 -0500

    SkinShifter

---
+++ skinshifter/js/playground.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+	var width = $(window).width();
+
+	if (width > '1300') {
+	   $('#leftpanel').css('display', 'block');
+	   $('#rightpanel').css('display', 'block');
+	   $('#toppanel').css('display', 'none');
+	   $('#bottompanel').css('display', 'none');
+	   $('#bottomexp').css('display', 'none');
+	}	
+
+	if (width < '1300') {
+	   $('#leftpanel').css('display', 'none');
+	   $('#rightpanel').css('display', 'none');
+	   var topwidth = $('#toppanel').width();
+	   var ratio = 60/1000;
+	   var topheight = topwidth * ratio;
+	   $('#toppanel').css('height', ''+ topheight +'px');	   
+	   $('#toppanel').slideDown( "slow" );
+
+	   var botwidth = $('#bottompanel').width();
+	   var ratio = 90/1000;
+	   var botheight = botwidth * ratio;	   
+	   $('#bottompanel').css('height', ''+ botheight +'px');
+
+	   var botexpwidth = $('#bottomexp').width();
+	   var ratio = 225/1000;
+	   var botexpheight = botexpwidth * ratio;	   
+	   $('#bottomexp').css('height', ''+ botexpheight +'px');	   
+	}
+
+ 	$('#bottompanel').click(function(){
+ 		$('#bottomexp').slideDown('slow');
+ 		$('#bottompanel').slideUp('slow');
+ 	});
+
+ 	$('#bottomexp').click(function(){
+ 		$('#bottomexp').slideUp('slow');
+ 		$('#bottompanel').slideDown('slow');
+ 	}); 	
+
+});
+
+$( window ).resize(function() {
+	var width = $(window).width();
+
+	if (width > '1300') {
+	   $('#leftpanel').css('display', 'block');
+	   $('#rightpanel').css('display', 'block');
+	   $('#toppanel').css('display', 'none');
+	   $('#bottompanel').css('display', 'none');
+	   $('#bottomexp').css('display', 'none');
+	}	
+
+	if (width < '1300') {
+	   $('#leftpanel').css('display', 'none');
+	   $('#rightpanel').css('display', 'none');
+	   $('#toppanel').css('display', 'block');
+	   $('#bottompanel').css('display', 'block');
+
+	   var topwidth = $('#toppanel').width();
+	   var ratio = 60/1000;
+	   var topheight = topwidth * ratio;
+
+	   $('#toppanel').css('height', ''+ topheight +'px');
+
+	   var botwidth = $('#bottompanel').width();
+	   var ratio = 90/1000;
+	   var botheight = botwidth * ratio;	   
+	   $('#bottompanel').css('height', ''+ botheight +'px');
+
+	   var botexpwidth = $('#bottomexp').width();
+	   var ratio = 225/1000;
+	   var botexpheight = botexpwidth * ratio;	   
+	   $('#bottomexp').css('height', ''+ botexpheight +'px');
+	}
+
+});
